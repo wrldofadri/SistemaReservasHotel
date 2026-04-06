92fea7c6943490c0cd0f84f6112a93ec0ce2ce10
Lazzo <dragondeverde@gmail.com>
2026-04-06 18:27:19 -0400

    Dashboard administrativo con totales y gráficos completado

---
+++ SistemaReservasHotel-main/SistemaReservasHotel/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SistemaReservasHotel.Data;
+using SistemaReservasHotel.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+// 1. Configurar la conexión a la Base de Datos
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// 2. Configurar Identity (Usuarios y Roles)
+builder.Services.AddIdentity<AppUser, IdentityRole>(options => {
+    options.SignIn.RequireConfirmedAccount = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+// 3. Configurar las rutas para cuando el usuario no esté logueado
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Cuenta/Login";
+    options.AccessDeniedPath = "/Cuenta/AccesoDenegado";
+});
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+var app = builder.Build();
+// Ejecutar el Seeder de Roles y Admin
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        await DbSeeder.SeedRolesAndAdminAsync(services);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error al sembrar la base de datos: {ex.Message}");
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
